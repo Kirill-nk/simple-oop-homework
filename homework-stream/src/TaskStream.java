77f0e2e897c712fa496cb54d413a4f44a7e748de
Kirill-nk <kirill9414@yandex.ru>
2024-12-08 16:29:43 +0300

    Task 5,6

--- homework-stream/src/TaskStream.java
+++ homework-stream/src/TaskStream.java
@@ -76,12 +76,12 @@ public class TaskStream {
                 .collect(Collectors.toList());
     }
 
-        /**
-         * Определить среднюю стоимость книги (не считая сумму книг)
-         *
-         * @param books - список книг
-         * @return среднюю стоимость книги
-         */
+    /**
+     * Определить среднюю стоимость книги (не считая сумму книг)
+     *
+     * @param books - список книг
+     * @return среднюю стоимость книги
+     */
     public static double task6(List<Book> books) {
         return books.stream()
                 .mapToDouble(book -> book.getPrice())
@@ -89,64 +89,66 @@ public class TaskStream {
     }
     //Функциональные интерфейсы - посмотреть
 
-        /**
-         * У всех книг в поле Автор есть слово "Автор"
-         *
-         * @param books - список книг
-         * @return результат
-         */
+    /**
+     * У всех книг в поле Автор есть слово "Автор"
+     *
+     * @param books - список книг
+     * @return результат
+     */
     public static boolean task7(List<Book> books) {
-        return false;
+        return books.stream()
+                .filter(book -> book.getAuthor().contains("Автор"))
+                .collect(Collectors())
     }
 
-        /**
-         * Преобразовать List в Set. где Set это все названия книг. достаточно 3
-         *
-         * @param books - список книг
-         * @return не больше 3 названий книг
-         */
+    /**
+     * Преобразовать List в Set. где Set это все названия книг. достаточно 3
+     *
+     * @param books - список книг
+     * @return не больше 3 названий книг
+     */
 //    public static Set<String> task8(List<Book> books) {
 //        return Collections.emptySet();
 //    }
 
-        /**
-         * Найти книги, у которых в названии четная цифра, но цена меньше 100
-         *
-         * @param books - список книг
-         * @return
-         */
+    /**
+     * Найти книги, у которых в названии четная цифра, но цена меньше 100
+     *
+     * @param books - список книг
+     * @return
+     */
 //    public static List<Book> task9(List<Book> books) {
 //        return Collections.emptyList();
 //    }
 
-        /**
-         * поместить книга в Map по двум ключам: "OK" и "Not Ok". где второе когда цена у книги больше 50
-         *
-         * @param books - список книг
-         * @return Map с двумя ключами
-         */
+    /**
+     * поместить книга в Map по двум ключам: "OK" и "Not Ok". где второе когда цена у книги больше 50
+     *
+     * @param books - список книг
+     * @return Map с двумя ключами
+     */
 //    public static Map<String, List<Book>> task10(List<Book> books) {
 //        return Collections.emptyMap();
 //    }
 
-        /**
-         * Получить список книг, у которых хотя бы один отзыв содержит слово "рекомендую".
-         *
-         * @param books - список книг
-         * @return список книг с интересными отзывами
-         */
+    /**
+     * Получить список книг, у которых хотя бы один отзыв содержит слово "рекомендую".
+     *
+     * @param books - список книг
+     * @return список книг с интересными отзывами
+     */
 //    public static List<Book> task11(List<Book> books) {
 //        return Collections.emptyList();
 //    }
 
-        /**
-         * Найти самую дешевую книгу.
-         *
-         * @param books - список книг
-         * @return самая дешевая книга
-         */
+    /**
+     * Найти самую дешевую книгу.
+     *
+     * @param books - список книг
+     * @return самая дешевая книга
+     */
 //    public static Book task12(List<Book> books) throws IllegalArgumentException {
 //        return null;
 //    }
-    }
 }
+
